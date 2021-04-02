5ac958ac455347d9f870f949cceee179e0be6ac2
Trequend <68006090+Trequend@users.noreply.github.com>
2021-04-02 16:02:58 +0300

    Change SceneObject UI

--- ParticleSystem/src/objects/SceneObject.cpp
+++ ParticleSystem/src/objects/SceneObject.cpp
@@ -7,22 +7,20 @@ void SceneObject::Update(float deltaTime) { }
 
 void SceneObject::UI()
 {
-	ImGui::Text(name.c_str());
-
 	glm::vec3 translation = transform.GetTranslation();
-	if (ImGui::InputFloat3("Translation", &translation[0]))
+	if (ImGui::DragFloat3("Translation", &translation[0]))
 	{
 		transform.SetTranslation(translation);
 	}
 
 	glm::vec3 rotation = transform.GetRotation();
-	if (ImGui::InputFloat3("Rotation", &rotation[0]))
+	if (ImGui::DragFloat3("Rotation", &rotation[0]))
 	{
 		transform.SetRotation(rotation);
 	}
 
 	glm::vec3 scale = transform.GetScale();
-	if (ImGui::InputFloat3("Scale", &scale[0]))
+	if (ImGui::DragFloat3("Scale", &scale[0]))
 	{
 		transform.SetScale(scale);
 	}
