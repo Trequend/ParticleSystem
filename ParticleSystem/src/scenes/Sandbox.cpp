0a40cad348cc570840e8641718fbb8d973b8bcfb
Trequend <68006090+Trequend@users.noreply.github.com>
2021-04-03 11:20:20 +0300

    Change the example in Sandbox

--- ParticleSystem/src/scenes/Sandbox.cpp
+++ ParticleSystem/src/scenes/Sandbox.cpp
@@ -7,22 +7,27 @@
 void Sandbox::Initialize()
 {
 	AddObject(new ParticleSystemCPU("Particle system", 5000, 5, {
-		15.0f,                       // lifespan
-		0.0f,                        // lifespan variation
-		glm::vec3(0.0f),             // position
-		glm::vec3(0.2f),             // position variation
-		glm::vec3(1.0f, 1.0f, 0.0f), // velocity
-		glm::vec3(1.5f, 1.5f, 0.0f), // velocity variation
-		glm::vec3(0.0f),             // rotation
-		glm::vec3(0.0f),             // rotation variation
-		glm::vec3(0.0f),             // angular velocity
-		glm::vec3(0.0f),             // angular velocity variation
-		glm::vec4(1.0f),             // color begin
-		glm::vec4(0.0f),             // color end
-		1.0f,                        // size begin
-		0.0f,                        // size end
-		0.0f                         // size variation
+		7.0f,                                                      // lifespan
+		0.0f,                                                      // lifespan variation
+		glm::vec3(0.0f),                                           // position
+		glm::vec3(0.0f, 0.5f, 0.0f),                               // position variation
+		glm::vec3(1.0f, 1.0f, 0.0f),                               // velocity
+		glm::vec3(1.0f, 1.0f, 0.0f),                               // velocity variation
+		glm::vec3(0.0f),                                           // rotation
+		glm::vec3(0.0f),                                           // rotation variation
+		glm::vec3(0.0f),                                           // angular velocity
+		glm::vec3(0.0f, 0.0f, 15.0f),                              // angular velocity variation
+		glm::vec4(205.0f / 255, 169.0f / 255, 250.0f / 255, 1.0f), // color begin
+		glm::vec4(71.0f / 255, 169.0f / 255, 1.0f, 1.0f),          // color end
+		0.5f,                                                      // size begin
+		0.0f,                                                      // size end
+		0.0f                                                       // size variation
 	}));
+
+	Camera& camera = GetCamera();
+	camera.SetTranslation({ 8.0f, 8.0f, 0.0f });
+	camera.SetOrhographicProperties(2);
+
 	std::cout << "Initialize" << std::endl;
 }
 
